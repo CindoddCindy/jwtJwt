c57178eb4b83ff8879d8731e52aa64ec147224f0
CindoddCindy <cindodcindy@gmail.com>
2020-11-24 19:56:32 +0700

    tambah kelas role

--- cindodjwtjwt/src/main/java/com/cindodjwt/cindodjwtjwt/model/Role.java
+++ cindodjwtjwt/src/main/java/com/cindodjwt/cindodjwtjwt/model/Role.java
@@ -1,4 +1,41 @@
 package com.cindodjwt.cindodjwtjwt.model;
 
+
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_role; // belum jelas ini buat apa
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 60)
+    private RoleName name;
+
+    public  Role(){
+
+    }
+
+
+    public Long getId_role() {
+        return id_role;
+    }
+
+    public void setId_role(Long id_role) {
+        this.id_role = id_role;
+    }
+
+    public RoleName getName() {
+        return name;
+    }
+
+    public void setName(RoleName name) {
+        this.name = name;
+    }
 }
