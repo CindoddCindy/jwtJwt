a054d55cd8d9ff33133ff95d6db7450914e973ef
CindoddCindy <cindodcindy@gmail.com>
2020-11-26 11:57:01 +0700

    masih di user detail service

--- cindodjwtjwt/src/main/java/com/cindodjwt/cindodjwtjwt/security/CustomDetailServiceBuyer.java
+++ cindodjwtjwt/src/main/java/com/cindodjwt/cindodjwtjwt/security/CustomDetailServiceBuyer.java
@@ -1,6 +1,8 @@
 package com.cindodjwt.cindodjwtjwt.security;
 
+import com.cindodjwt.cindodjwtjwt.model.Buyer;
 import com.cindodjwt.cindodjwtjwt.model.Seller;
+import com.cindodjwt.cindodjwtjwt.repository.BuyerRepository;
 import com.cindodjwt.cindodjwtjwt.repository.SellerRepository;
 import com.sun.security.auth.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +13,28 @@ import org.springframework.transaction.annotation.Transactional;
 
 public class CustomDetailServiceBuyer implements UserDetailsService {
 
+    @Autowired
+    BuyerRepository buyerRepository;// dari repo
+
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+        // Let people login with either username or email
+        Buyer buyer = buyerRepository.findBuyerByUsernameOrEmail(s, s)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username or email : " + s)
+                );
+
+        return UserPrincipal.create(buyer);
+       // return null;
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        Buyer buyer = buyerRepository.findById(id).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with id : " + id)
+        );
+
+        return UserPrincipal.create(buyer);
     }
 }
--- cindodjwtjwt/src/main/java/com/cindodjwt/cindodjwtjwt/security/CustomDetailServiceSeller.java
+++ cindodjwtjwt/src/main/java/com/cindodjwt/cindodjwtjwt/security/CustomDetailServiceSeller.java
@@ -10,10 +10,31 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 
 public class CustomDetailServiceSeller implements UserDetailsService {
+    @Autowired
+    SellerRepository sellerRepository;// dari repo
+
 
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+
+        // Let people login with either username or email
+        Seller seller = sellerRepository.findSellerByUsernameOrEmail(s, s)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username or email : " + s)
+                );
+
+        return UserPrincipal.create(seller);
+
+       // return null;
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        Seller seller = sellerRepository.findById(id).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with id : " + id)
+        );
+
+        return UserPrincipal.create(seller);
     }
 }
